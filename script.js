428e90fb4617ed0481c97a8b77d89e7a8c610818
MariaMariscal <maria.mariscal@uc.cl>
2021-05-31 13:22:11 -0400

    cambio 3

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 //GAME IDEA: You sing and if ur notes match the notes of the karaoke song you get coins. It is part social and part game bc you can share/follow but also use ur coins to unlock premium songs and stuff. You can also write ur own songs for other people to try and you can join (maybe live) competitions
 
-const profileImgs = [
-  "https://images.unsplash.com/photo-1546190255-451a91afc548?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=162&h=162",
-  "https://images.unsplash.com/photo-1593626986521-f24d25d60103?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=162&h=162",
-  "https://images.unsplash.com/photo-1581467655410-0c2bf55d9d6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=162&h=162",
-  "https://images.unsplash.com/photo-1581467655410-0c2bf55d9d6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&auto=format&fit=crop&w=162&h=162",
-  "https://images.unsplash.com/photo-1432251407527-504a6b4174a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&auto=format&fit=crop&w=162&h=162",
-  "https://images.unsplash.com/photo-1589199584418-9fb838d95188?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&auto=format&fit=crop&w=162&h=162",
-  "https://images.unsplash.com/photo-1499464796999-4ced935b55da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&auto=format&fit=crop&w=162&h=162",
-  "https://images.unsplash.com/placeholder-avatars/extra-large.jpg?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff"
-];
-const profileImage = document.getElementById("profileimg");
-
-function imgChange() {
-  profileImage.src = profileImgs[Math.round(Math.random() * 8)];
-}
-
-imgChange();
-
 const posts = document.querySelectorAll(".gallery-item");
 
 posts.forEach((post) => {
@@ -28,7 +10,6 @@ posts.forEach((post) => {
     window.open(imgUrl, "_blank");
   });
 });
-
 window.onLoad = function () {
   var profileUserName = document.getELementById("profile-user-name");
   profileUserName.innerHTML = username;
@@ -39,3 +20,8 @@ window.onLoad = function () {
   var profileBioText = document.getELementById("profile-bio-text");
   profileBioText.innerHTML = yourBio;
 };
+
+function popup() {
+  popup = document-getELementById("myPopup"); 
+  popup.classList.toggle("show");
+}
